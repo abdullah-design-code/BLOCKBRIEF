925423e37a3407927c12384aa583d70a8c5f6e26
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-21 20:46:20 +0500

    Create sanity.ts

---
+++ src/lib/sanity.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+  projectId: 'vo1tl79z',
+  dataset: 'production',
+  apiVersion: '2024-01-01',
+  useCdn: true
+})
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source: any) => builder.image(source)

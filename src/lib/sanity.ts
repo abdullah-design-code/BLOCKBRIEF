23eaf252bb96c8a84c837730d88daad32eac1fda
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-23 21:23:50 +0500

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -5,9 +5,9 @@ export const client = createClient({
   projectId: 'vo1tl79z',
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: false
+  useCdn: false,
+  token: import.meta.env.VITE_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(client)
-
 export const urlFor = (source: any) => builder.image(source)

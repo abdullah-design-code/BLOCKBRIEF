ba1a1b286bada7bc54d6c82f44af260b6f7506da
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-26 13:54:51 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import NewsPage from "./pages/NewsPage";
 import ArticlePage from "./pages/ArticlePage";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminLogin from "./pages/admin/AdminLogin";
 
-export default function Root() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/news/:slug" element={<ArticlePage />} />
+    <Routes>
 
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      {/* Public Routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/news" element={<NewsPage />} />
+      <Route path="/news/:slug" element={<ArticlePage />} />
 
-      </Routes>
-    </BrowserRouter>
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+    </Routes>
   );
 }

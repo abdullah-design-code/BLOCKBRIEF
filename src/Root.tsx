4ad435f9dbeb3d3f09263e28a344e1bf13c64a85
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-26 14:40:37 +0500

    Refactor Root component and simplify routing

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,30 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-
 import Home from "./pages/Home";
 import NewsPage from "./pages/NewsPage";
 import ArticlePage from "./pages/ArticlePage";
-
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminLogin from "./pages/admin/AdminLogin";
 
-export default function Root() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/news/:slug" element={<ArticlePage />} />
 
-        <Route element={<App />}>
-          {/* PUBLIC */}
-          <Route path="/" element={<Home />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/news/:slug" element={<ArticlePage />} />
-
-          {/* ADMIN */}
-          <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        </Route>
-
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;

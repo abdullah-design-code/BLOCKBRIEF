f00b1c66e78ac58dc3c8251b4f69fc4d16c6ea82
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-26 14:25:12 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import Home from "./pages/Home";
 import NewsPage from "./pages/NewsPage";
 import ArticlePage from "./pages/ArticlePage";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminLogin from "./pages/admin/AdminLogin";
 
-export default function App() {
+export default function Root() {
   return (
-    <div style={{ color: "white", padding: 20 }}>
-      BLOCKBRIEF IS RUNNING ✅
-    </div>
-  );
-}
+    <BrowserRouter>
+      <Routes>
+        {/* Public */}
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="news" element={<NewsPage />} />
+          <Route path="news/:slug" element={<ArticlePage />} />
+        </Route>
 
-      {/* Public Routes */}
-      <Route path="/" element={<Home />} />
-      <Route path="/news" element={<NewsPage />} />
-      <Route path="/news/:slug" element={<ArticlePage />} />
-
-      {/* Admin Routes */}
-      <Route path="/admin" element={<AdminLogin />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-
-    </Routes>
+        {/* Admin */}
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }

4bb186e5cd986e8233ce3b74ffaabd98ee6d01db
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-26 14:46:54 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import Root from "./Root"; // IMPORTANT
+import Root from "./Root";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <Root />
-  </HelmetProvider>
+  <Root />
 );

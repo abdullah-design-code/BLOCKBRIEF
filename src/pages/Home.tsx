45db61b4c084ccbe46d5c24c0c54b35b4636cde2
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-26 14:37:44 +0500

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import NewsFeed from "@/components/NewsFeed";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ padding: "20px", fontSize: "24px" }}>
+        BLOCKBRIEF Home
+      </h1>
+
+      <NewsFeed />
+    </div>
+  );
+};
+
+export default Home;

705086e54f9d39885fa733f373487904da2dc21b
abdullah-design-code <tunioabdullah965@gmail.com>
2026-04-26 14:32:38 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,26 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Home from "./pages/Home";
-import NewsPage from "./pages/NewsPage";
-import ArticlePage from "./pages/ArticlePage";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminLogin from "./pages/admin/AdminLogin";
+import { Outlet } from "react-router-dom";
 
-export default function Root() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public */}
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="news" element={<NewsPage />} />
-          <Route path="news/:slug" element={<ArticlePage />} />
-        </Route>
-
-        {/* Admin */}
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-background text-foreground">    
+      <Outlet />
+    </div>
   );
 }
+
+export default App;
